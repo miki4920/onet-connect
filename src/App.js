a623a4279e2c1fab0ca1b7a0629d3a26d7cb5831
M Grobelny <366-mg448@users.noreply.campus.cs.le.ac.uk>
2022-12-09 17:54:32 +0000

    Added empty and full tiles

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import React from 'react';
 import {v4 as uuidv4} from 'uuid';
 import './App.css';
 
-
+function randomiseArray(array) {
+    let currentIndex = array.length, randomIndex;
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+    return array;
+}
 
 class Tile {
     constructor() {
@@ -11,12 +20,10 @@ class Tile {
 }
 
 class Board extends React.Component {
-
-
     randomiseTiles(tileQueue) {
         while (tileQueue.length > 0) {
             let image = this.icons[Math.floor(Math.random() * this.icons.length)]
-            for(let i = 0; i < 2; i++) {
+            for (let i = 0; i < 2; i++) {
                 tileQueue.pop().image = image
             }
         }
@@ -25,15 +32,16 @@ class Board extends React.Component {
     createBoard() {
         let board = []
         let queue = []
-        for(let y = 0; y < this.boardHeight + 2; y++) {
+        for (let y = 0; y < this.boardHeight + 2; y++) {
             let row = []
-            for(let x = 0; x < this.boardWidth + 2; x++) {
+            for (let x = 0; x < this.boardWidth + 2; x++) {
                 let tile = new Tile()
                 row.push(tile)
                 queue.push(tile)
             }
             board.push(row)
         }
+        queue = randomiseArray(queue)
         this.randomiseTiles(queue)
         return board
     }
@@ -42,23 +50,26 @@ class Board extends React.Component {
         super(props);
         this.boardWidth = 6;
         this.boardHeight = 12;
-        this.icons = ["bone", "book", "boot", "brain", "eyes", "gloves", "heart", "helmet", "key", "knife", "letter",
-                      "papyrus", "purse", "scroll", "skull", "stake", "tooth"]
+        this.icons = ["animal_skull", "arrow", "bone", "book", "boot", "brain", "crown", "doll", "eyes", "gloves",
+            "heart", "helmet", "key", "knife", "letter", "papyrus", "potion", "purse", "scroll", "skull", "stake",
+            "tooth", ""]
         this.board = this.createBoard()
 
     }
 
     render() {
+
         return (
             <main>
                 {this.board.map((row) => (
                     <div key={uuidv4()} className="row">
                         {row.map((tile) =>
+
                             <button
                                 key={uuidv4()}
-                                className="tile"
+                                className={"tile" + (tile.image ? " hasTile" : "")}
                                 style={{
-                                    background: `url("icons/${tile.image}.png")`,
+                                    backgroundImage: `url("icons/${tile.image}.png")`,
                                     backgroundSize: "cover"
                                 }}>
                             </button>
