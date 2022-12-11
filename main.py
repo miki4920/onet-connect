a3e713f58edad96d762f1870f9b7b3ed685aef5f
miki4920 <miki4920@gmail.com>
2022-12-11 01:45:18 +0000

    Implemented python hosting

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+from flask import Flask, send_from_directory
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+application = Flask(__name__, static_url_path="", static_folder="build", template_folder="build")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@application.route("/")
+def index():
+    return send_from_directory(application.static_folder, "index.html")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+if __name__ == "__main__":
+    application.run()
